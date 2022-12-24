23c7cdb9d84ef292ba730bfef8d7e88e12472c39
Mohammed Aman Nawaz <95523603+Nawaz-25@users.noreply.github.com>
2022-12-25 01:57:19 +0530

    Updates

---
+++ precache-manifest.87c891feb91fc3ed3e17874cb8cb8e6e.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0039810d898699dd3f33e9618d9d8b19",
+    "url": "/meme-app/index.html"
+  },
+  {
+    "revision": "90a4b93aed25e29e78c8",
+    "url": "/meme-app/static/css/2.ce357e74.chunk.css"
+  },
+  {
+    "revision": "90a4b93aed25e29e78c8",
+    "url": "/meme-app/static/js/2.aef583b4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/meme-app/static/js/2.aef583b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e12e35224dbf89d5eea",
+    "url": "/meme-app/static/js/main.704fd4cf.chunk.js"
+  },
+  {
+    "revision": "70d4b9a4a413e75906cb",
+    "url": "/meme-app/static/js/runtime-main.be2723cc.js"
+  },
+  {
+    "revision": "3b675fd3f2027d613a548daa94aa5a0a",
+    "url": "/meme-app/static/media/app-icon.3b675fd3.svg"
+  },
+  {
+    "revision": "24f7b0944e9e03a905f9d7701573b2cd",
+    "url": "/meme-app/static/media/open-sans-latin-300.24f7b094.woff2"
+  },
+  {
+    "revision": "a7622f60c56ddd5301549a786b54e6e6",
+    "url": "/meme-app/static/media/open-sans-latin-300.a7622f60.woff"
+  },
+  {
+    "revision": "6725c3fb1f3abd7ebdcb2d244758f706",
+    "url": "/meme-app/static/media/open-sans-latin-300italic.6725c3fb.woff2"
+  },
+  {
+    "revision": "793b1237017aeacd646fb80911425566",
+    "url": "/meme-app/static/media/open-sans-latin-300italic.793b1237.woff"
+  },
+  {
+    "revision": "33543c5cc5d88f5695dd08c87d280dfd",
+    "url": "/meme-app/static/media/open-sans-latin-400.33543c5c.woff2"
+  },
+  {
+    "revision": "de0869e324680c99efa1250515b4b41c",
+    "url": "/meme-app/static/media/open-sans-latin-400.de0869e3.woff"
+  },
+  {
+    "revision": "06b4bfda4e139eaf3ab9872a6d66f42f",
+    "url": "/meme-app/static/media/open-sans-latin-400italic.06b4bfda.woff"
+  },
+  {
+    "revision": "b61a9055d92ac56c15cacf4271d4f072",
+    "url": "/meme-app/static/media/open-sans-latin-400italic.b61a9055.woff2"
+  },
+  {
+    "revision": "449d681cd6006390e1bee3c3a660430b",
+    "url": "/meme-app/static/media/open-sans-latin-600.449d681c.woff"
+  },
+  {
+    "revision": "819af3d3abdc9f135d49b80a91e2ff4c",
+    "url": "/meme-app/static/media/open-sans-latin-600.819af3d3.woff2"
+  },
+  {
+    "revision": "56e5756b696615d6164a625e1bcb1a9e",
+    "url": "/meme-app/static/media/open-sans-latin-600italic.56e5756b.woff"
+  },
+  {
+    "revision": "ebdfa24cb411ba3016abd1c3ea5746e5",
+    "url": "/meme-app/static/media/open-sans-latin-600italic.ebdfa24c.woff2"
+  },
+  {
+    "revision": "0edb76284a7a0f8db4665b560ee2b48f",
+    "url": "/meme-app/static/media/open-sans-latin-700.0edb7628.woff2"
+  },
+  {
+    "revision": "1f85e92d8ff443980bc0f83ad7b23b60",
+    "url": "/meme-app/static/media/open-sans-latin-700.1f85e92d.woff"
+  },
+  {
+    "revision": "a49393f2171a2f4d6927ce6c14f73dbc",
+    "url": "/meme-app/static/media/open-sans-latin-700italic.a49393f2.woff2"
+  },
+  {
+    "revision": "bf72679ca22e53320beaea090e8bb07d",
+    "url": "/meme-app/static/media/open-sans-latin-700italic.bf72679c.woff"
+  },
+  {
+    "revision": "05ebdbe10796850f045fcd484f35788d",
+    "url": "/meme-app/static/media/open-sans-latin-800.05ebdbe1.woff"
+  },
+  {
+    "revision": "ab3e500e7375695d702cff19513e4470",
+    "url": "/meme-app/static/media/open-sans-latin-800.ab3e500e.woff2"
+  },
+  {
+    "revision": "92da6f116d973bd334cf9b3afdb29c4f",
+    "url": "/meme-app/static/media/open-sans-latin-800italic.92da6f11.woff"
+  },
+  {
+    "revision": "c62c3eea77cf3fca51494e8c924adf4d",
+    "url": "/meme-app/static/media/open-sans-latin-800italic.c62c3eea.woff2"
+  }
+]);
